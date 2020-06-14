dca991f6cf02273baca704b022915cefee1e4767
RodinYTY <1418583869@qq.com>
2020-06-14 23:44:02 +0800

    🚩注册界面

--- signup.cpp
+++ signup.cpp
@@ -1,5 +1,6 @@
 #include "signup.h"
 #include "ui_signup.h"
+#define CODE_MAX_LEN 6
 
 SignUp::SignUp(QWidget *parent) :
     QWidget(parent),
@@ -8,6 +9,7 @@ SignUp::SignUp(QWidget *parent) :
     ui->setupUi(this);
     ui->lbl5->setVisible(false);
     ui->code->setVisible(false);
+    ui->major->setVisible(false);
 }
 
 SignUp::~SignUp()
@@ -20,9 +22,17 @@ void SignUp::on_role_currentIndexChanged(int index)
     if(index == 0){
         ui->lbl5->setVisible(false);
         ui->code->setVisible(false);
+        ui->major->setVisible(false);
     }
     else{
         ui->lbl5->setVisible(true);
         ui->code->setVisible(true);
+        ui->major->setVisible(true);
     }
+}
+
+void SignUp::on_code_textChanged(const QString &arg1)
+{
+    if(arg1.length() > CODE_MAX_LEN)
+        ui->code->setText(arg1.left(CODE_MAX_LEN));
 }
--- signup.h
+++ signup.h
@@ -19,6 +19,8 @@ public:
 private slots:
     void on_role_currentIndexChanged(int index);
 
+    void on_code_textChanged(const QString &arg1);
+
 private:
     Ui::SignUp *ui;
 };
